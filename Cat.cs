392521c552f7bba02d47cb7ed2280e787a20ff6a
Minkyung Jeong <s2335@e-mirim.hs.kr>
2025-06-30 13:58:29 +0900

    메서드 sealed 예

--- Cat.cs
+++ Cat.cs
@@ -4,7 +4,7 @@ namespace ClassExample31
 {
     public class Cat : Animal
     {
-        public override void Eat()
+        public sealed override void Eat()
         {
             Console.WriteLine("고양이 사료를 먹습니다.");
         }
